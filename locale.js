edb6916308f47d2589b3bc15b6efa64d3c306565
Yui San ae2f <84163330+yuisanae2f@users.noreply.github.com>
2023-12-31 12:02:41 +0900

    Update locale.js

--- locale.js
+++ locale.js
@@ -1,21 +1,20 @@
 function locale() {
+    let aa = args();
     let l = new tent("locale", "div", document.body);
     let lh = l.tent("title", "h2");
-    lh.set({ko: "언어", en: "Languages"}[args()[0]]);
-
-    let aa = args();
+    lh.set({ko: "언어", en: "Languages"}[aa[0]]);
 
     if(args()[0] != 'ko') {
         aa[0] = 'ko';
         let ko = l.tab("ko", "div").tent("a", "a");
         ko.set("한국어");
-        ko.pos().href = getURL(argt(aa));
+        ko.pos().href = `${window.location.href.split('?')[0]}?${btoa(argt(aa))}`;
     }
 
     if(args()[0] != 'en') {
         aa[0] = 'en';
         let en = l.tab("en", "div").tent("a", "a");
         en.set("English");
-        en.pos().href = getURL(argt(aa));
+        en.pos().href = `${window.location.href.split('?')[0]}?${btoa(argt(aa))}`;
     }
 }
